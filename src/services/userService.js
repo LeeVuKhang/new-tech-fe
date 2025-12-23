aae41ec785f533c6eeacafbce8b2877b1285e6da
LeeVuKhang <lvukhangpq@gmail.com>
2025-12-23 17:06:41 +0700

    fiix user api url

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://new-tech-be.onrender.com/api/v1';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://new-tech-be.onrender.com/api/v1';
 
 // Create axios instance with default config
 const axiosInstance = axios.create({
