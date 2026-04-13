ddb902440d4e83202bd700db221981535c48631a
LeeVuKhang <lvukhangpq@gmail.com>
2026-04-14 00:54:32 +0700

    retry https config

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.at75jeonco"
+    "revision": "0.lit2o1799b8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
